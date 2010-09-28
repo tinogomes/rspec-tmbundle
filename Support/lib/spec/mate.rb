7a5dd4e2d0d0a9932fa0240f90c74d935435208c
Jim Meyer <jim@geekdaily.org>
2010-09-28 14:08:06 -0700

    Patching mate.rb to find rspec in vendor/gems as well as vendor/plugins

--- Support/lib/spec/mate.rb
+++ Support/lib/spec/mate.rb
@@ -1,20 +1,28 @@
 # This is based on Florian Weber's TDDMate
 
 ENV['TM_PROJECT_DIRECTORY'] ||= File.dirname(ENV['TM_FILEPATH'])
-rspec_rails_plugin = File.join(ENV['TM_PROJECT_DIRECTORY'],'vendor','plugins','rspec','lib')
 
+def has_spec_autorun?(directory)
+  File.exists?(File.join(directory, %w(spec autorun.rb)))
+end
+
+# Load spec/autorun
 if File.exist?(File.join(ENV['TM_PROJECT_DIRECTORY'], 'Gemfile'))
   require "rubygems"
   require "bundler"
   Bundler.setup
-elsif File.directory?(rspec_rails_plugin)
-  $LOAD_PATH.unshift(rspec_rails_plugin)
-elsif ENV['TM_RSPEC_HOME']
-  rspec_lib = File.join(ENV['TM_RSPEC_HOME'], 'lib')
-  unless File.directory?(rspec_lib)
-    raise "TM_RSPEC_HOME points to a bad location: #{ENV['TM_RSPEC_HOME']}"
+else
+  # Find it the old-fashioned way: in vendor/plugins, or in vendor/gems, 
+  # or at TM_RSPEC_HOME
+  rspec_locs = Dir.glob(File.join(ENV['TM_PROJECT_DIRECTORY'],'vendor','{plugins,gems}','rspec{,-[0-9]*}', 'lib'))
+  rspec_locs << File.join(ENV['TM_RSPEC_HOME'], 'lib') if ENV['TM_RSPEC_HOME']
+  rspec_root = rspec_locs.reject { |dir| !has_spec_autorun?(dir) }.first
+
+  if rspec_root
+    $LOAD_PATH.unshift(rspec_root)
+  elsif ENV['TM_RSPEC_HOME']
+    raise "TM_RSPEC_HOME points to a bad location: #{ENV['TM_RSPEC_HOME']}" unless File.directory?(File.join(ENV['TM_RSPEC_HOME'], 'lib'))
   end
-  $LOAD_PATH.unshift(rspec_lib)
 end
 
 begin

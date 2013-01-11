ac024bf604e48a1e65c85e53e8dfa0cb3bf4c353
Jason Perkins <jperkins@me.com>
2013-01-11 17:23:30 -0500

    mate.rb refactoring
    * extracted the following methods:
    
    - use_bundler?
    - find_rspec_lib
    - gemfile?
    
    - bundler_option?
    - skip_bundler_option?
    
    * bundler_option? and skip_bundler_option? can be moved to Options

--- Support/lib/rspec/mate.rb
+++ Support/lib/rspec/mate.rb
@@ -1,27 +1,68 @@
 # This is based on Florian Weber's TDDMate
 
 ENV['TM_PROJECT_DIRECTORY'] ||= File.dirname(ENV['TM_FILEPATH'])
+
 $LOAD_PATH.unshift(File.dirname(__FILE__) + '/..')
+
 require 'rspec/mate/runner'
 require 'rspec/mate/options'
 require 'rspec/mate/switch_command'
 
 rspec_lib = nil
 
+# TODO: move to Options
+def bundler_option?
+  RSpec::Mate::Options['--bundler']
+end
+
+# TODO: move to Options
+def skip_bundler_option?
+  RSpec::Mate::Options['--skip-bundler']
+end
+
+def find_rspec_lib
+  candidate_rspec_lib_paths = Dir.glob(
+    File.join(
+      ENV['TM_PROJECT_DIRECTORY'],
+      'vendor',
+      '{plugins,gems}',
+      '{rspec,rspec-core}{,-[0-9]*}',
+      'lib'
+    )
+  )
+
+  if ENV['TM_RSPEC_HOME']
+    candidate_rspec_lib_paths << File.join(
+      ENV['TM_RSPEC_HOME'],
+      'lib'
+    )
+  end
+
+  rspec_lib = candidate_rspec_lib_paths.detect do |dir|
+    File.exist?(dir)
+  end
+end
+
 def gemfile?
   File.exist?(File.join(ENV['TM_PROJECT_DIRECTORY'], 'Gemfile'))
 end
 
-if RSpec::Mate::Options['--bundler'] || (gemfile? && !RSpec::Mate::Options['--skip-bundler'])
+def use_bundler?
+  bundler_option? || (gemfile? && skip_bundler_option?)
+end
+
+
+if use_bundler?
   require "rubygems"
   require "bundler"
+
   Bundler.setup
 else
-  candidate_rspec_lib_paths = Dir.glob(File.join(ENV['TM_PROJECT_DIRECTORY'],'vendor','{plugins,gems}','{rspec,rspec-core}{,-[0-9]*}', 'lib'))
-  candidate_rspec_lib_paths << File.join(ENV['TM_RSPEC_HOME'], 'lib') if ENV['TM_RSPEC_HOME']
-  rspec_lib = candidate_rspec_lib_paths.detect { |dir| File.exist?(dir) }
+  rspec_lib = find_rspec_lib
 
-  $LOAD_PATH.unshift(rspec_lib) if rspec_lib
+  if rspec_lib
+    $LOAD_PATH.unshift(rspec_lib)
+  end
 end
 
 if RSpec::Mate::Options['--rspec-version']
@@ -47,4 +88,3 @@ end
 def rspec2?
   defined?(RSpec::Core)
 end
-

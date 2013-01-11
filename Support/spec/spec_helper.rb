5966779689764fe6942933c09d461a6ddb063823
Jason Perkins <jperkins@me.com>
2013-01-11 17:18:25 -0500

    adding simplecov

--- Support/spec/spec_helper.rb
+++ Support/spec/spec_helper.rb
@@ -1,3 +1,11 @@
+require 'simplecov'
+
+SimpleCov.start do
+  add_filter '/converage/'
+  add_filter '/fixtures/'
+  add_filter '/spec/'
+end
+
 ENV['TM_PROJECT_DIRECTORY'] ||= '.'
 
 require 'stringio'
